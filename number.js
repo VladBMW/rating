1b29ee5871abee0d06dbadaf1298d8353dd58a05
VladBMW <PrograMMerJS@yandex.ua>
2021-01-25 09:37:20 +0300

    rating
    rating

---
+++ number.js
@@ -0,0 +1,49 @@
+let vm = new Vue({
+    el: "#app" ,
+    data: {
+        check: true,
+        checked: true,
+        rating: 5,
+        items: 5
+    
+    }
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
